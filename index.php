1f1bb04f5d064101c3e805a0661d85516419c98e
YS-PC\youichi <nokia1216@gmail.com>
2021-06-12 22:17:13 +0900

    URL変更

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
-		$uri = 'http://';
+		$uri = 'http://efefe';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/dashboard/');
